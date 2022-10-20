78da42e926d408823fafe8896d6019e93b88c707
Kapcha <t.kozhevnikov@g.nsu.ru>
2022-10-20 23:48:04 +0700

    Update 1

--- main.cpp
+++ main.cpp
@@ -11,26 +11,30 @@ class Decimal {
     bool sign = false;
 
     Decimal& sum(const Decimal&);
-    Decimal& diff(const Decimal&);
+    Decimal& diff(const Decimal&, bool flag);
 
     void reverse();
 public:
     Decimal();
-    //Decimal(const Decimal&);
     Decimal(int);
     Decimal(std::string);
     ~Decimal();
 
-    //bool operator>=(const Decimal&) const;
-
-    //const Decimal abs() const;
-
-    //friend std::ostream& operator<<(std::ostream& os, const Decimal& dec);
+    bool operator==(const Decimal&) const;
+    bool operator>=(const Decimal&) const;
     Decimal& operator=(const Decimal&);
     Decimal& operator+=(const Decimal&);
+    Decimal& operator-=(const Decimal&);
+    Decimal& operator*=(const Decimal&);
+    //Decimal& operator/=(const Decimal&);
     const Decimal operator+(const Decimal&);
+    const Decimal operator-(const Decimal&);
+    const Decimal operator*(const Decimal&);
+    //const Decimal operator/(const Decimal&);
 
     void PrintValue();
+    void PrintSign();
+    void PrintDecimal();
 };
 
 void Decimal::reverse() {
@@ -83,6 +87,28 @@ Decimal::~Decimal() {
     delete [] value;
 }
 
+bool Decimal::operator==(const Decimal& val) const {
+    if (!std::strcmp(value, val.value)) {
+        return true;
+    }
+    return false;
+}
+
+bool Decimal::operator>=(const Decimal &val) const {
+    if (!std::strcmp(value, val.value)) {
+        return true;
+    }
+    int max_length = length >= val.length ? length : val.length;
+
+    for (int i = max_length - 1; i >= 0; i--) {
+        if (value[i] > val.value[i])
+            return true;
+        else if (value[i] != val.value[i])
+            return false;
+    }
+    return false;
+}
+
 Decimal& Decimal::operator=(const Decimal& val) {
     if (this == &val)
         return *this;
@@ -99,30 +125,72 @@ Decimal& Decimal::sum(const Decimal& val) {
     char toAdd = 0;
     size_t max_size = (allocated >= val.allocated ? allocated : val.allocated);
     size_t max_length = (length >= val.length ? length : val.length);
-
+    char* res = new char[max_length];
+    strcpy(res, value);
+    delete [] value;
+    value = res;
     for(int i = 0; i < max_length; i++) {
         int tmp = (i < length ? INT(value[i]) : 0) +
-                (i < val.length ? INT(val.value[i]) : 0) + toAdd;
+                  (i < val.length ? INT(val.value[i]) : 0) + toAdd;
         toAdd = tmp / 10;
         value[i] = CHAR(tmp % 10);
     }
     if (toAdd) {
         if (max_size == max_length) {
-            char* res = new char[max_length + 1];
-            strcpy(res, value);
-            res[max_length] = '1';
-            delete value;
-            value = res;
+            char* _res = new char[max_length + 1];
+            strcpy(_res, value);
+            _res[max_length] = '1';
+            delete [] value;
+            value = _res;
+            length++;
+            allocated++;
             return *this;
         }
         value[max_length] = '1';
+        length = max_length;
+        allocated = max_length;
         return *this;
     }
+    length = max_length;
+    allocated = max_length;
     return *this;
 }
 
-Decimal& Decimal::diff(const Decimal& val) {
-    ;
+Decimal& Decimal::diff(const Decimal& val, bool flag) {
+    char toSub = 0;
+    size_t max_length = (length >= val.length ? length : val.length);
+    char *res = new char[max_length];
+    bool key = false;
+    if (*this >= val)
+        key = true;
+    for(int i = 0; i < max_length; i++) {
+        int tmp = (key ? 1 : -1) * ((i < length ? INT(value[i]) : 0) -
+                                    (i < val.length ? INT(val.value[i]) : 0)) - toSub;
+        if (tmp < 0) {
+            tmp += 10;
+            toSub = 1;
+        } else
+            toSub = 0;
+        res[i] = CHAR(tmp);
+    }
+    int i = max_length - 1;
+    while (res[i] == '0' && i > 0) {
+        max_length--;
+        res[i] = '\0';
+        i--;
+    }
+    char *_res = new char[max_length];
+    strcpy(_res, res);
+    delete [] res;
+    delete [] value;
+    value = _res;
+    length = max_length;
+    allocated = max_length;
+    if (!key && flag)
+        sign = val.sign;
+    if (!key && !flag)
+        sign = !val.sign;
+    return *this;
 }
 
 Decimal& Decimal::operator+=(const Decimal& val) {
@@ -130,7 +198,7 @@ Decimal& Decimal::operator+=(const Decimal& val) {
         return sum(val);
     }
     else {
-        return diff(val);
+        return diff(val, true);
     }
 }
 
@@ -141,47 +209,133 @@ const Decimal Decimal::operator+(const Decimal & val) {
     return tmp;
 }
 
-void Decimal::PrintValue() {
-    reverse();
-    std::cout << value << std::endl;
+Decimal& Decimal::operator-=(const Decimal& val) {
+    if (sign != val.sign) {
+        return sum(val);
+    }
+    else {
+        return diff(val, false);
+    }
 }
 
+const Decimal Decimal::operator-(const Decimal & val) {
+    Decimal tmp;
+    tmp = *this;
+    tmp -= val;
+    return tmp;
+}
+
+Decimal& Decimal::operator*=(const Decimal &val) {
+    if((length == 1 && value[0] == '0') || (val.length == 1 && val.value[0] == '0')){
+        *this = Decimal();
+        return *this;
+    }
+    char *arr = new char[length + val.length];
+    for (int i = 0; i < length; i++)
+        for (int j = 0; j < val.length; j++){
+            arr[i + j] += INT(value[i]) * INT(val.value[j]);
+        }
+    length += val.length;
+    char *res = new char[length];
+    delete [] value;
+    value = res;
+    for (int tmp, i = 0, t = 0; i < length; i++)
+    {
+        tmp = t + arr[i];
+        arr[i] = tmp % 10;
+        t = tmp / 10;
+        value[i] = CHAR(arr[i]) ;
+    }
+    for (int i = length - 1; i >= 1 && !arr[i]; i--) {
+        value[i] = '\0';
+        length--;
+    }
+    char *_res = new char[length];
+    strcpy(_res, value);
+    delete [] value;
+    value = _res;
+    allocated = length;
+    if (sign == val.sign)
+        sign = false;
+    else
+        sign = true;
+    return *this;
+}
+
+const Decimal Decimal::operator*(const Decimal & val) {
+    Decimal tmp;
+    tmp = *this;
+    tmp *= val;
+    return tmp;
+}
 /*
-bool Decimal::operator>=(const Decimal &val) const {
-    if (std::strcmp(value, val.value)) {
-        return true;
+Decimal& Decimal::operator/=(const Decimal &val) {
+    if (val.length == 1 && val.value[0] == '0')
+        throw("Division By 0");
+    if (!(*this >= val)) {
+        *this = Decimal();
+        return *this;
     }
-    int maxSize = allocated >= val.allocated ? allocated : val.allocated;
-
-    for (int i = maxSize - 1; i >= 0; i++) {
-        if (value[i] > val.value[i])
-            return true;
-        else if (value[i] != val.value[i])
-            return false;
+    if(*this == val) {
+        *this = Decimal(1);
+        return *this;
     }
-
-    return false;
+    int i, lgcat = 0, cc;
+    char *res = new char[length];
+    Decimal tmp;
+    Decimal tmp2;
+    Decimal tmp3;
+    tmp3 = val;
+    for (i = length - 1; tmp2 = tmp * 10, !(tmp2 + INT(value[i]) >= tmp3); i--) {
+        tmp *= 10;
+        tmp += INT(value[i]);
+    }
+    for (; i >= 0; i--) {
+        tmp *= 10;
+        tmp += INT(value[i]);
+        for (cc = 9; !(tmp >= tmp3 * cc); cc--);
+        tmp -= tmp3 * cc;
+        res[lgcat++] = cc;
+    }
+    delete value;
+    value = res;
+    reverse();
+    if (sign == val.sign)
+        sign = false;
+    else
+        sign = true;
+    return *this;
 }
 
-std::ostream &operator<<(std::ostream &os, const Decimal &dec) {
-    return os << dec.value;
-}
-
-Decimal::Decimal(const Decimal & dec) {
-    sign = dec.sign;
-    allocated = dec.allocated;
-    value = new char[allocated];
-    strcpy(value, dec.value);
-    length = dec.length;
+const Decimal Decimal::operator/(const Decimal & val) {
+    Decimal tmp;
+    tmp = *this;
+    tmp /= val;
+    return tmp;
 }
 */
+void Decimal::PrintValue() {
+    value[length] = '\0';
+    reverse();
+    std::cout << value << std::endl;
+    reverse();
+}
+
+void Decimal::PrintSign() {
+    std::cout << (sign ? '-' : '+');
+}
+
+void Decimal::PrintDecimal() {
+    PrintSign();
+    PrintValue();
+}
 
 int main() {
-    Decimal a("123");
-    Decimal b("47");
+    Decimal a("100");
+    Decimal b("12");
     Decimal d;
-    Decimal c(12);
-    a = a + b;
-    a.PrintValue();
+    Decimal c(1200);
+    a = d * c;
+    a.PrintDecimal();
     return 0;
 }
